153f721a8b82e748d744962edea56a826799b109
johncolani <johnhcolani@gmail.com>
2024-06-28 22:43:26 -0400

    Added Entire UI

--- app/src/main/java/com/example/myapplication/WeatherPage.kt
+++ app/src/main/java/com/example/myapplication/WeatherPage.kt
@@ -1,19 +1,32 @@
 package com.example.myapplication
-
-import android.graphics.drawable.Icon
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.SegmentedButtonDefaults.Icon
+import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
@@ -22,57 +35,186 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.myapplication.api.NetworkResponse
+import com.example.myapplication.api.WeatherModel
+import kotlin.coroutines.CoroutineContext
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WeatherPage(viewModel: WeatherViewModel) {
     var city by remember {
         mutableStateOf("")
     }
     val weatherResult = viewModel.weatherResult.observeAsState()
-    Column (
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Row (
+            .fillMaxSize()
+            .padding(WindowInsets.systemBars.asPaddingValues())
+
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img) ,
+            contentDescription =null,
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop
+            )
+
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ){
-         OutlinedTextField(
-             modifier = Modifier.weight(1f),
-             value = city,
-             onValueChange = {
-                 city = it
-             },
-             label = {
-                 Text(text="Search for any Location")
-             }
-             )
-            IconButton(onClick = {
-                viewModel.getData(city)
-            }) {
-                  Icon(imageVector = Icons.Default.Search,
-                contentDescription = "Search for my location"
-                  )
+                .padding(vertical = 16.dp, horizontal = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+
+            ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                OutlinedTextField(
+
+                    modifier = Modifier.weight(1f),
+
+                    value = city,
+                    onValueChange = {
+                        city = it
+                    },
+                    label = {
+                        Text(text = "Search for any Location", color = Color.White)
+                    },
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedTextColor = Color.White,
+                        focusedBorderColor = Color.White,
+                        unfocusedBorderColor = Color.White,
+                        cursorColor = Color.White
+                    )
+                )
+                IconButton(onClick = {
+                    viewModel.getData(city)
+                    city = ""
+
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search for my location",
+                        tint = Color.White
+                    )
+                }
+            }
+            when (val result = weatherResult.value) {
+                is NetworkResponse.Error -> {
+                    Text(text = result.message)
+                }
+
+                NetworkResponse.Loading -> {
+                    CircularProgressIndicator()
+                }
+
+                is NetworkResponse.Success -> {
+                    WeatherDetail(data = result.data)
+                }
+
+                null -> {}
             }
         }
-        when(val result = weatherResult.value){
-            is NetworkResponse.Error -> {
-                Text(text = result.message)
+    }
+}
+
+@Composable
+fun WeatherDetail(modifier: Modifier = Modifier,data:WeatherModel) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Icon(
+                    imageVector = Icons.Default.LocationOn,
+                    contentDescription = "Location icon",
+                    modifier = Modifier.size(40.dp),
+                    tint = Color.Cyan
+                )
+                Text(text = data.location.name, fontSize = 24.sp, color = Color.White)
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = data.location.country, fontSize = 14.sp, color = Color.White)
             }
-            NetworkResponse.Loading -> {
-                CircularProgressIndicator()
+
+            Text(
+                text = "${data.current.temp_f}°f",
+                fontSize = 56.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                color = Color(0xFFFFA500)
+            )
+            AsyncImage(
+                modifier = Modifier.size(160.dp),
+                model = "https:${data.current.condition.icon}",
+                contentDescription = "Condition Icon"
+            )
+
+            Text(
+                text = data.current.condition.text,
+                fontSize = 20.sp,
+
+                textAlign = TextAlign.Center,
+                color = Color.Gray
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Card {
+                Column(
+                    modifier = Modifier.fillMaxWidth(),
+
+                    ) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ){
+                      WeatherKeyVal("Humidity", data.current.humidity)
+                      WeatherKeyVal("Wind Speed", data.current.wind_mph)
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ){
+                        WeatherKeyVal("UV", data.current.uv)
+                        WeatherKeyVal("Participation", data.current.precip_in)
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ){
+                        WeatherKeyVal("Local Time", data.location.localtime.split(" ")[1])
+                        WeatherKeyVal("Local Date", data.location.localtime.split(" ")[0])
+                    }
+
+                }
             }
-            is NetworkResponse.Success -> {
-                Text(text = result.data.toString())
-            }
-            null -> {}
         }
+    }
+
+@Composable
+fun WeatherKeyVal(key: String, value: String) {
+    Column (
+        modifier = Modifier.padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Text(text = key, fontWeight = FontWeight.SemiBold, color = Color.Gray)
     }
 }
